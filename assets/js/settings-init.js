388ec5c2d1e2f115061c0f1429e2b6e1312444c9
Rainer Mbongo <rainermbongo@gmail.com>
2024-09-25 02:41:26 +0300

    files

---
+++ assets/js/settings-init.js
@@ -0,0 +1,12 @@
+"use strict"
+var optionSettings = {
+    layout:"wide",
+    background:"dark",
+    color:"green",
+    header:"fixed",
+    font:"opensans",
+    textDirection:"ltr",
+    radius:"sixradius",
+    showSettings:"show",
+};
+new antlerSettings(optionSettings);
